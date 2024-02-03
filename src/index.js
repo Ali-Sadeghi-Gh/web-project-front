9ef7f6ccdd04226bf3586341f6db71e52b2622c1
Alibanayeean <a.banayeean@gmail.com>
2024-02-03 22:49:33 +0330

    remove shop

--- src/index.js
+++ src/index.js
@@ -6,17 +6,13 @@ import "bootstrap/dist/css/bootstrap.css";
 import "remixicon/fonts/remixicon.css";
 import "./index.css";
 
-import store from "./store/store";
-import { Provider } from "react-redux";
 
 import { BrowserRouter as Router } from "react-router-dom";
 
 ReactDOM.render(
   <React.StrictMode>
     <Router>
-      <Provider store={store}>
-        <App />
-      </Provider>
+      <App />
     </Router>
   </React.StrictMode>,
   document.getElementById("root")
